886b7aa8f9ab95def70d73b544e781732cdd9765
MichOchieng <42423172+MichOchieng@users.noreply.github.com>
2020-01-31 14:50:01 -0800

    Fixed xor example

--- SAM4S Xpro Bare Metal/SAM4S Xpro Bare Metal/start.s
+++ SAM4S Xpro Bare Metal/SAM4S Xpro Bare Metal/start.s
@@ -7,29 +7,35 @@
 .section .text
 .thumb
 .type start, %function
-start:
-	mov r0, #0x01			/* This is a comment */
-	mov r1, #0x02
-	mov r2, #0x03
+start:	
 	/* Question 1 */
-	mov r3,r0
+	/*mov r3,r0
 	mov r0,r1
 	mov r1,r3
+	b .*/
 	/* Question 2 */
 	mov r0, #0b01
 	mov r1, #0b01
-	and r2,r0,r1		
-	mvn r4,r0
-	mvn r5,r1
-	or r4,r5
+	mov r2, #0b01
+	mov r3, #0b01
+
+	and r0,r1
+	mvn r0
+
+	mvn r2
+	mvn r3
+	and r2,r3	
 	b .
 	/*Question 3*/
-	mov r0, #0b01
+	/*	In the first step of this algorithm the xor of r0 (10) and r1 (01) is take and then stored in r0 (new value is 11) then in
+		the second step r1 is xor'd with r0 (resulting in 10 being stored in r1) and finally r0 is xor'd one more time with r1
+		resulting in 01 being put in r0. r0 is now 01 (r1's initial value) and r1 is now 10 (r0's initial value). The registers have
+		successfully swapped values.
+	*/
+	/*mov r0, #0b10
 	mov r1, #0b01
 
-	mvn r2,r0
-	mvn r3,r1
-
-	and r0,r3
-	and r2,r1
-	b .
+	eor r0,r1
+	eor r1,r0
+	eor r0,r1
+	b .*/
